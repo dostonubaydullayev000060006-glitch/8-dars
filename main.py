4fb7c4d33d64e384386ff6b7355743a41c03cdfc
Doston <dostonubaydullayev00006.0006@gmail.com>
2026-05-06 17:15:07 +0500

    darsdagi kod

---
+++ main.py
@@ -0,0 +1,103 @@
+import os
+os.system("cls")
+
+# try:
+#     a = int(input("Son1: "))
+#     b = int(input("Son2: "))
+
+#     print(a / b)
+
+# except ValueError:
+#     print("Xatolik: Faqat son kriting!")
+# except ZeroDivisionError:
+#     print("Xatolik: Sonni nolga bo'lish mumkin emas!")
+# else:
+#     print("Dastur bajarildi!")
+# finally:
+#     print("Try except tugadi!")
+
+
+
+                                #   MODUL
+
+# import os
+# os.system("cls")
+
+# import math
+
+# print(math.pow(5, 3))
+
+# print(math.sqrt(25))
+
+# print(math.log2(16))
+# print(math.log(625, 5))
+
+# print(math.floor(2.95))
+# print(math.ceil(4.01))
+# print(round(2.95), round(4.51))
+ 
+  
+
+#                                        RANDOM
+
+# import os
+# os.system("cls")
+
+# import random as rd
+
+# n = rd.randint(1,100)
+# print(n)
+
+# # m = rd.uniform(1, 10)
+# # print(m)
+
+# names = ['Samandar', 'Sherali', 'Doston', 'Begzod', 'Suhrob']
+# print(rd.choice(names))
+# print(rd.choices(names, k=2))
+# print(rd.sample(names, k=2))
+
+# rd.shuffle(names)
+# print(names)
+
+
+   
+#                                           Os MODULI
+
+
+# import os
+# os.system("cls")
+
+# os.mkdir("salom")
+# os.rmdir("Salom")
+
+# os.remove("temp")
+# os.rename("XOgame.c","gameXO.py")
+
+
+            #            wikipedia    dan ma'lumot qidirish     
+
+
+# import os 
+# os.system("cls")
+
+# import wikipedia as wk 
+# wk.set_lang("uz")
+# page = wk.page("Najot ta'lim")
+# print(page.content)
+
+
+
+#                         Tarjima qilish
+
+
+# import os
+# os.system("cls")
+
+# from translate import Translator
+
+# tarjimon = Translator(to_lang = "tr",from_lang="uz")
+# text = tarjimon.translate("Salom Doston yaxshimsan , qale bulopti kurslar")
+# print(text)
+
+
+ 
